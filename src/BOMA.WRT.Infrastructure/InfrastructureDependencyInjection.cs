3c2cb7308bc286526d51aa0d9500b724d6fda8f9
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-06-23 19:07:09 +0200

    Create Infrastructure project and move infrastructure components initialization there

---
+++ src/BOMA.WRT.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BOMA.WRT.Infrastructure;
+
+public static class InfrastructureDependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static IApplicationBuilder AddInfrastructure(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        
+        return app;
+    }
+}
--- src/BOMA.WTR.Api/Program.cs
+++ src/BOMA.WTR.Api/Program.cs
@@ -1,25 +1,13 @@
+using BOMA.WRT.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.AddInfrastructure(app.Environment);
 app.MapControllers();
 
 app.Run();
