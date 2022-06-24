4066354621c9023bbcc00b62ffa002541354e43a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-06-24 07:40:24 +0200

    Parse CSV to the model

--- src/BOMA.WTR.Application/Hangfire/ParseWorkingTimeRecordsFileJob.cs
+++ src/BOMA.WTR.Application/Hangfire/ParseWorkingTimeRecordsFileJob.cs
@@ -1,4 +1,7 @@
-﻿using BOMA.WRT.Application.RogerFiles;
+﻿using System.Globalization;
+using BOMA.WRT.Application.RogerFiles;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Hangfire.Console;
 using Hangfire.Server;
 using Microsoft.Extensions.Options;
@@ -16,14 +19,38 @@ public class ParseWorkingTimeRecordsFileJob
     
     public async Task Execute(PerformContext? context, CancellationToken cancellationToken)
     {
-        if (Directory.GetFiles(_rogerFileOptions.FileLocation, "*.csv").Length < 1)
+        var files = Directory.GetFiles(_rogerFileOptions.FileLocation, "*.csv");
+        
+        if (files.Length < 1)
         {
             context.WriteLine("There is no CSV files to get");
             return;
         }
-        
-        
+
+        foreach (var file in files)
+        {
+            var rogerFileModels = ParseCsvToRogerFile(file);
+            rogerFileModels = rogerFileModels.Where(x => x.IsValid()).ToList();
+            
+            context.WriteLine($"There are {rogerFileModels.Count()} valid entries in the file - {file}");
+
+            // TODO: add entries to DB
+        }
 
         return;
+    }
+
+    private static IEnumerable<RogerFileModel> ParseCsvToRogerFile(string fileLocation)
+    {
+        using var reader = new StreamReader(fileLocation);
+        using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";",
+            HasHeaderRecord = true,
+            MissingFieldFound = null,
+            TrimOptions = TrimOptions.Trim
+        });
+        
+        return csv.GetRecords<RogerFileModel>().ToList();
     }
 }
---
+++ src/BOMA.WTR.Application/RogerFiles/RogerFileModel.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace BOMA.WRT.Application.RogerFiles;
+
+public class RogerFileModel
+{
+    [Index(0)]
+    public string? RogerEventName { get; set; }
+    
+    [Index(1)]
+    public RogerEventType? RogerEventType { get; set; }
+    
+    [Index(2)]
+    public string? UserId { get; set; }
+    
+    [Index(3)]
+    public string? Name { get; set; }
+    
+    [Index(4)]
+    public string? LastName { get; set; }
+    
+    [Index(5)]
+    public int? UserRcpId { get; set; }
+    
+    [Index(6)]
+    public DateTime? Date { get; set; }
+    
+    [Index(7)]
+    public TimeSpan? Time { get; set; }
+    
+    [Index(8)]
+    public int? GroupId { get; set; }
+    
+    [Index(9)]
+    public string? GroupName { get; set; }
+
+    public bool IsValid() => RogerEventType is not RogerFiles.RogerEventType.None && !string.IsNullOrEmpty(UserId);
+}
+
+public enum RogerEventType
+{
+    Entry = 0,
+    Exit = 16,
+    None = 32
+}
