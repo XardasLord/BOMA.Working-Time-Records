06b5d13b8b73505e91994af68dd889a8d5a08223
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-06-29 08:59:24 +0200

    Add and register MediatR nuget

--- src/BOMA.WTR.Api/Program.cs
+++ src/BOMA.WTR.Api/Program.cs
@@ -1,8 +1,10 @@
+using BOMA.WRT.Application;
 using BOMA.WRT.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
---
+++ src/BOMA.WTR.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BOMA.WRT.Application;
+
+public static class ApplicationDependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+        => services.AddMediatR(Assembly.GetExecutingAssembly());
+}
