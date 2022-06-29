3b576149644dacbd26ad1544628e9823dfe53b39
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-06-29 10:11:18 +0200

    Add MediatR post and pre request logger

---
+++ src/BOMA.WTR.Application/MediatorRequestPipelines/PreRequestLogger.cs
@@ -0,0 +1,24 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace BOMA.WTR.Application.MediatorRequestPipelines;
+
+public class PreRequestLogger<TRequest> : IRequestPreProcessor<TRequest> 
+    where TRequest : notnull
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public PreRequestLogger(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Process(TRequest request, CancellationToken cancellationToken = default)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        _logger.LogInformation("Request started: {requestName}", requestName);
+
+        return Task.CompletedTask;
+    }
+}
