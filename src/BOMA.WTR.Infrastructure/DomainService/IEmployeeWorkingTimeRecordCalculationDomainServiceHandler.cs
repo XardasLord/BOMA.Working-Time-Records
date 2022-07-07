ad277044f5e3f97c6348aefacae859a7021e46a8
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-07-07 08:31:09 +0200

    Adjust calculation handler to new DateTime normalitzation based on business rules

--- src/BOMA.WTR.Infrastructure/DomainService/IEmployeeWorkingTimeRecordCalculationDomainServiceHandler.cs
+++ src/BOMA.WTR.Infrastructure/DomainService/IEmployeeWorkingTimeRecordCalculationDomainServiceHandler.cs
@@ -12,12 +12,13 @@ public class EmployeeWorkingTimeRecordCalculationDomainService : IEmployeeWorkin
 
         var previousDay = 0;
         var aggregatedMinutesForDay = 0d;
-        DateTime previousDate = default;
-        RecordEventType previousEventType = RecordEventType.None;
+        var previousDate = default(DateTime);
+        var previousEventType = RecordEventType.None;
         
         foreach (var timeRecord in workingTimeRecords)
         {
             var currentDay = timeRecord.OccuredAt.Day;
+            var normalizedOccuredAt = NormalizeDateTime(timeRecord.EventType, timeRecord.OccuredAt);
 
             if (currentDay > previousDay && previousDay != 0) // Is a next day
             {
@@ -35,7 +36,7 @@ public class EmployeeWorkingTimeRecordCalculationDomainService : IEmployeeWorkin
                 } else if (previousEventType == RecordEventType.Entry && timeRecord.EventType == RecordEventType.Exit)
                 {
                     // TODO: Add that difference between previous date and current date
-                    aggregatedMinutesForDay += (int)(timeRecord.OccuredAt - previousDate).TotalMinutes;
+                    aggregatedMinutesForDay += (int)(normalizedOccuredAt - previousDate).TotalMinutes;
                     
                     result.Add(new WorkingTimeRecordAggregatedViewModel
                     {
@@ -47,8 +48,8 @@ public class EmployeeWorkingTimeRecordCalculationDomainService : IEmployeeWorkin
                     
                     aggregatedMinutesForDay = 0;
                     previousEventType = timeRecord.EventType;
-                    previousDay = timeRecord.OccuredAt.Day;
-                    previousDate = timeRecord.OccuredAt;
+                    previousDay = normalizedOccuredAt.Day;
+                    previousDate = normalizedOccuredAt;
                     
                     continue;
                 }
@@ -64,14 +65,14 @@ public class EmployeeWorkingTimeRecordCalculationDomainService : IEmployeeWorkin
                 
                 if (timeRecord.EventType == RecordEventType.Exit)
                 {
-                    aggregatedMinutesForDay += (int)(timeRecord.OccuredAt - previousDate).TotalMinutes;
+                    aggregatedMinutesForDay += (int)(NormalizeDateTime(timeRecord.EventType, normalizedOccuredAt) - previousDate).TotalMinutes;
                 }
             }
 
             
             previousEventType = timeRecord.EventType;
-            previousDay = timeRecord.OccuredAt.Day;
-            previousDate = timeRecord.OccuredAt;
+            previousDay = normalizedOccuredAt.Day;
+            previousDate = normalizedOccuredAt;
         }
 
         if (aggregatedMinutesForDay > 0)
@@ -86,5 +87,58 @@ public class EmployeeWorkingTimeRecordCalculationDomainService : IEmployeeWorkin
         }
         
         return result;
+    }
+
+    private static DateTime NormalizeDateTime(RecordEventType recordEventType, DateTime dateTime)
+    {
+        switch (recordEventType)
+        {
+            case RecordEventType.Entry:
+                // Entries to work at 5:40-6:05 then system records it as 6:00
+                if (dateTime.Minute is >= 40 or <= 5)
+                {
+                    return new DateTime(
+                        dateTime.Year,
+                        dateTime.Month,
+                        dateTime.Day,
+                        dateTime.Minute is >= 0 and <= 5 ? dateTime.Hour : dateTime.AddHours(1).Hour,
+                        0,
+                        0);
+                }
+                
+                // Entries to work at 6:06-6:39 then system records it as 6:30
+                return new DateTime(
+                    dateTime.Year,
+                    dateTime.Month,
+                    dateTime.Day,
+                    dateTime.Hour,
+                    30,
+                    0);
+            case RecordEventType.Exit:
+                // Exists work at 13:55-14:24 then system records it as 14:00
+                if (dateTime.Minute is >= 55 or <= 24)
+                {
+                    return new DateTime(
+                        dateTime.Year,
+                        dateTime.Month,
+                        dateTime.Day,
+                        dateTime.Minute is >= 55 and <= 59 ? dateTime.AddHours(1).Hour : dateTime.Hour,
+                        0,
+                        0);
+                }
+                
+                // Exists work at 14:25-14:54 then system records it as 14:30
+                return new DateTime(
+                    dateTime.Year,
+                    dateTime.Month,
+                    dateTime.Day,
+                    dateTime.Hour,
+                    30,
+                    0);
+            case RecordEventType.None:
+                return dateTime;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(recordEventType), recordEventType, null);
+        }
     }
 }
