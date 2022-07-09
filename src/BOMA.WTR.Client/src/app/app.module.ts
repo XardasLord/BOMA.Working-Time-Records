885c929bda2395f11a0aef6dc972f54020096c81
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-07-09 21:01:41 +0200

    Register local data pl-PL on client

--- src/BOMA.WTR.Client/src/app/app.module.ts
+++ src/BOMA.WTR.Client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,10 +6,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
+import localePl from '@angular/common/locales/pl';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePl);
+
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, BrowserAnimationsModule, CoreModule, SharedModule],
-	providers: [],
+	providers: [{ provide: LOCALE_ID, useValue: 'pl-PL' }],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
