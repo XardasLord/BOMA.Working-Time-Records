62d9786f8b941c8a38c809e10f0f3fed5848b577
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-08-10 18:20:46 +0200

    Unit tests for domain UpdateData logic

---
+++ src/tests/BOMA.WTR.Domain.Tests.Unit/AggregateModels/UpdateDataTests.cs
@@ -0,0 +1,45 @@
+﻿using BOMA.WTR.Domain.AggregateModels;
+using BOMA.WTR.Domain.AggregateModels.ValueObjects;
+using BOMA.WTR.Tests.Base;
+using FluentAssertions;
+using Xunit;
+
+namespace BOMA.WTR.Domain.Tests.Unit.AggregateModels;
+
+public class UpdateDataTests : TestBase
+{
+    private readonly Employee _employee;
+    private Name _name;
+    private Money _salary;
+    private PercentageBonus _percentageBonus;
+    private int _rcpId;
+    private int _departmentId;
+
+    public UpdateDataTests()
+    {
+        _employee = Create<Employee>();
+    }
+    
+    private void Act() => _employee.UpdateData(_name, _salary, _percentageBonus, _rcpId, _departmentId);
+
+    [Fact]
+    public void update_data_sets_new_values()
+    {
+        // Arrange
+        _name = Create<Name>();
+        _salary = Create<Money>();
+        _percentageBonus = Create<PercentageBonus>();
+        _rcpId = CreateInt();
+        _departmentId = CreateInt();
+        
+        // Act
+        Act();
+        
+        // Assert
+        _employee.Name.Should().Be(_name);
+        _employee.Salary.Should().Be(_salary);
+        _employee.SalaryBonusPercentage.Should().Be(_percentageBonus);
+        _employee.RcpId.Should().Be(_rcpId);
+        _employee.DepartmentId.Should().Be(_departmentId);
+    }
+}
