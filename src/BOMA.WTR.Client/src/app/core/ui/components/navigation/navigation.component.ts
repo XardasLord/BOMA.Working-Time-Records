31332a24b0e81abb36764d8a41d29571b35e2a92
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-09-02 21:26:15 +0200

    Grant access to Employees tab for Manager mode application

--- src/BOMA.WTR.Client/src/app/core/ui/components/navigation/navigation.component.ts
+++ src/BOMA.WTR.Client/src/app/core/ui/components/navigation/navigation.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { environment } from '../../../../../environments/environment';
 
 @Component({
 	selector: 'app-navigation',
@@ -10,8 +9,6 @@ import { environment } from '../../../../../environments/environment';
 	styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-	managerMode = environment.managerMode;
-
 	isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
 		map((result) => result.matches),
 		shareReplay()
