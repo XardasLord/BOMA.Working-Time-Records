2fc65303795840bd8577536e404dffd09542aef9
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-03-03 10:07:46 +0100

    Change minimal salary for 2023

--- src/BOMA.WTR.Infrastructure/DomainService/EmployeeWorkingTimeRecordCalculationDomainServiceHandler.cs
+++ src/BOMA.WTR.Infrastructure/DomainService/EmployeeWorkingTimeRecordCalculationDomainServiceHandler.cs
@@ -309,7 +309,7 @@ public class EmployeeWorkingTimeRecordCalculationDomainService : IEmployeeWorkin
     public double GetNightFactorBonus(int year, int month)
     {
         var workedHoursInMonth = new DateTime(year, month, 1).WorkingHoursInMonthExcludingBankHolidays();
-        var nightFactor = 3010 / workedHoursInMonth * 0.2;
+        var nightFactor = 3490 / workedHoursInMonth * 0.2;
 
         return Math.Round(nightFactor, 2);
     }
