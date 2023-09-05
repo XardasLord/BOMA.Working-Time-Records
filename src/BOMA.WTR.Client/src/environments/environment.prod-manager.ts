c08bb2e5b591e5a86acfc878691d3c2d99775183
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-09-05 21:34:26 +0200

    Add build for Chef production

--- src/BOMA.WTR.Client/src/environments/environment.prod-manager.ts
+++ src/BOMA.WTR.Client/src/environments/environment.prod-manager.ts
@@ -2,7 +2,7 @@ import packageJson from '../../package.json';
 
 export const environment = {
 	production: true,
-	appVersion: `${packageJson.version}`,
+	appVersion: `${packageJson.version}-manager`,
 	apiEndpoint: 'http://192.168.0.161:81/api',
 	managerMode: true
 };
