0f1c696101f2f5da2d5c75e6b250b4557d799e7d
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-10-28 18:30:20 +0200

    Add reference to InsERT COM DLL

--- src/BOMA.WTR.Infrastructure/InsertGT/Gratyfikant/GratyfikantService.cs
+++ src/BOMA.WTR.Infrastructure/InsertGT/Gratyfikant/GratyfikantService.cs
@@ -1,7 +1,38 @@
-﻿namespace BOMA.WTR.Infrastructure.InsertGT.Gratyfikant
+﻿using BOMA.WTR.Infrastructure.InsertGT.Gratyfikant.Exceptions;
+
+namespace BOMA.WTR.Infrastructure.InsertGT.Gratyfikant
 {
     internal class GratyfikantService
     {
-        //var a = new InsERT.Gratyfikant();
+        public GratyfikantService()
+        {
+            
+        }
+
+        public void OpenGratyfikant()
+        {
+            var gratyfikant = RunGratyfikant();
+        }
+
+        private InsERT.Gratyfikant RunGratyfikant()
+        {
+            var gt = new InsERT.GT
+            {
+                Produkt = InsERT.ProduktEnum.gtaProduktGratyfikant,
+                Serwer = "localhost\\SQLEXPRESS",
+                Baza = "TEST_GRATYFIKANT",
+                Autentykacja = InsERT.AutentykacjaEnum.gtaAutentykacjaMieszana,
+                Uzytkownik = "sa",
+                Operator = "szef",
+                OperatorHaslo = ""
+            };
+
+            if (gt.Uruchom((int)InsERT.UruchomDopasujEnum.gtaUruchomDopasuj, (int)InsERT.UruchomEnum.gtaUruchom) is not InsERT.Gratyfikant gratyfikant)
+                throw new GratyfikantOperationException("Gratyfikant cannot be opened");
+            
+            gratyfikant.Okno.Widoczne = true;
+
+            return gratyfikant;
+        }
     }
 }
