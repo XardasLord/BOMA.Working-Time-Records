38de3dc7e39c69f6c662058095a2983bd6864a73
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2024-01-09 13:15:09 +0100

    Adjust environment variables

--- src/BOMA.WTR.Client/src/environments/environment.prod.ts
+++ src/BOMA.WTR.Client/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ import packageJson from '../../package.json';
 export const environment = {
 	production: true,
 	appVersion: `${packageJson.version}`,
+	identityEndpoint: 'http://192.168.0.161:81',
 	apiEndpoint: 'http://192.168.0.161:81/api',
 	managerMode: false
 };
