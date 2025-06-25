3653310f38b2e03187b0aa517699f84c9761ab7d
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2025-06-25 14:17:15 +0200

    Tabs refactoring

--- src/BOMA.WTR.Client/src/app/working-time-record/components/working-time-record-list/working-time-record-list.component.ts
+++ src/BOMA.WTR.Client/src/app/working-time-record/components/working-time-record-list/working-time-record-list.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../../environments/environment';
+import { Component, inject, OnInit } from '@angular/core';
 import { WorkingTimeRecord } from '../../state/working-time-record.action';
-import GetAll = WorkingTimeRecord.GetAll;
 import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthState } from '../../../shared/auth/state/auth.state';
 import { Role } from '../../../shared/auth/models/userDetails';
+import { WorkingTimeRecordDetailedTableComponent } from '../working-time-record-detailed-table/working-time-record-detailed-table.component';
+import { WorkingTimeRecordSummaryTableComponent } from '../working-time-record-summary-table/working-time-record-summary-table.component';
+import { WorkingTimeRecordReportHoursTableComponent } from '../working-time-record-report-hours-table/working-time-record-report-hours-table.component';
+import { WorkingTimeRecordReportHoursSummaryTableComponent } from '../working-time-record-report-hours-summary-table/working-time-record-report-hours-summary-table.component';
+import { WorkingTimeRecordReportAbsentsTableComponent } from '../working-time-record-report-absents-table/working-time-record-report-absents-table.component';
+import GetAll = WorkingTimeRecord.GetAll;
 
 @Component({
 	selector: 'app-working-time-record-list',
@@ -12,13 +18,52 @@ import { Role } from '../../../shared/auth/models/userDetails';
 	styleUrls: ['./working-time-record-list.component.scss']
 })
 export class WorkingTimeRecordListComponent implements OnInit {
+	private store = inject(Store);
+
+	allTabs: WorkingTimeTab[] = [
+		{
+			label: 'Szczegóły',
+			component: WorkingTimeRecordDetailedTableComponent,
+			visibleFor: [Role.Admin]
+		},
+		{
+			label: 'Podsumowanie',
+			component: WorkingTimeRecordSummaryTableComponent,
+			visibleFor: [Role.Admin]
+		},
+		{
+			label: 'Raport godzinowy',
+			component: WorkingTimeRecordReportHoursTableComponent,
+			visibleFor: []
+		},
+		{
+			label: 'Raport godzinowy - podsumowanie',
+			component: WorkingTimeRecordReportHoursSummaryTableComponent,
+			visibleFor: []
+		},
+		{
+			label: 'Raport nieobecności',
+			component: WorkingTimeRecordReportAbsentsTableComponent,
+			visibleFor: [Role.Admin, Role.User, Role.UserWithSalaryView, Role.UserWithSalaryEdit]
+		}
+	];
+
+	filteredTabs$!: Observable<WorkingTimeTab[]>;
 	Role = Role;
 
 	userRole$ = this.store.select(AuthState.getUserRole);
 
-	constructor(private store: Store) {}
-
 	ngOnInit(): void {
 		this.store.dispatch(new GetAll());
+
+		this.filteredTabs$ = this.userRole$.pipe(
+			map((role) => this.allTabs.filter((tab) => tab.visibleFor.length === 0 || tab.visibleFor.includes(role!)))
+		);
 	}
+}
+
+interface WorkingTimeTab {
+	label: string;
+	component: any;
+	visibleFor: Role[];
 }
