e291d435319d42178f4fd1ac522e0327d8682f2d
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2025-11-24 07:57:09 +0100

    Add percentage values for Salary Report

--- src/BOMA.WTR.Client/src/app/reports/components/salary-report/salary-report.component.ts
+++ src/BOMA.WTR.Client/src/app/reports/components/salary-report/salary-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, DecimalPipe } from '@angular/common';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { ReportsState } from '../../state/reports.state';
@@ -8,7 +8,7 @@ import { ReportDataModel } from '../../models/report-data.model';
 @Component({
 	selector: 'app-salary-report',
 	standalone: true,
-	imports: [AsyncPipe, CurrencyPipe],
+	imports: [AsyncPipe, CurrencyPipe, DecimalPipe],
 	templateUrl: './salary-report.component.html',
 	styleUrl: './salary-report.component.scss'
 })
@@ -16,4 +16,11 @@ export class SalaryReportComponent {
 	private store = inject(Store);
 
 	reportData$: Observable<ReportDataModel | null> = this.store.select(ReportsState.getReportData);
+
+	calculatePercentage(value: number | undefined, total: number | undefined): number {
+		if (!value || !total || total === 0) {
+			return 0;
+		}
+		return (value / total) * 100;
+	}
 }
