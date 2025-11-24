8fd41de93f47698cadc0cf1ec767517d39267aec
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2025-11-24 08:01:39 +0100

    Add print option for Reports

--- src/BOMA.WTR.Client/src/app/reports/pages/reports/reports.component.ts
+++ src/BOMA.WTR.Client/src/app/reports/pages/reports/reports.component.ts
@@ -5,11 +5,25 @@ import { WorkingHoursReportComponent } from '../../components/working-hours-repo
 import { SalaryReportComponent } from '../../components/salary-report/salary-report.component';
 import { Reports } from '../../state/reports.action';
 import LoadData = Reports.LoadData;
+import { MatFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import { NgIf } from '@angular/common';
+import { NgxPrintDirective } from 'ngx-print';
 
 @Component({
 	selector: 'app-reports',
 	standalone: true,
-	imports: [ReportFiltersComponent, WorkingHoursReportComponent, SalaryReportComponent],
+	imports: [
+		ReportFiltersComponent,
+		WorkingHoursReportComponent,
+		SalaryReportComponent,
+		MatFabButton,
+		MatIcon,
+		MatTooltip,
+		NgIf,
+		NgxPrintDirective
+	],
 	templateUrl: './reports.component.html',
 	styleUrl: './reports.component.scss'
 })
